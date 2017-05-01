b24bf0f2ba565b6e0e44948d28109cab74cff94b
Tiago Donascimento <tacoscorner@hotmail.com>
2017-05-01 11:37:20 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+  Rails.application.routes.draw do
+    devise_for :users
+
+    devise_scope :user do
+      authenticated :user do
+        root to: 'stories#index', as: :authenticated_root
+        resources :categories, only: [:index]
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+end
